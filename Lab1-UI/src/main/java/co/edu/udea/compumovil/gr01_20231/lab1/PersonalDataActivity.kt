ecf2783232d1b6bdc294ade68e1f3cdb83a643db
Mateo Aguirre <zaguirrez05@gmail.com>
2023-03-11 14:39:53 -0500

    mockup of the second view

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20231/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20231/lab1/PersonalDataActivity.kt
@@ -2,10 +2,18 @@ package co.edu.udea.compumovil.gr01_20231.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+
+        val next = findViewById<Button>(R.id.buttonNext)
+        next.setOnClickListener{
+            val intent = Intent(this, ContactDataActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
